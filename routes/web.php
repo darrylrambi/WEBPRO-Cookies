594be02549986f0c170e412dd7bf14c1a8ef725b
darrylrambi <darrylrambi@gmail.com>
2024-12-01 02:36:30 +0700

    set routes

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Default route untuk semua page
 Route::view('LoginPage', 'LoginPage')->name('LoginPage');
 Route::view('RegisterPage', 'RegisterPage')->name('RegisterPage');
+Route::view('LupaPasswordPage', 'LupaPasswordPage')->name('LupaPasswordPage');
+Route::view('MainPage', 'MainPage')->name('MainPage');
+
+
